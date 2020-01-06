bd5753c398d90cd3a242ef22a9b694e06258373b
mengyuan.tan <tanmy1016@outlook.com>
2020-01-06 11:12:41 +0800

    增加处理数据部分

--- Helper.py
+++ Helper.py
@@ -19,22 +19,19 @@ with open(CONFIG_FILE, 'r', encoding='utf-8') as f:
     FOV = config["FOV"]
     WALL_NODE_NUM = config["WALL_NODE_NUM"]
 
-
 # Resolve data
 try:
     room_size = list(map(int, ROOM_SIZE.split("*")))
+    ROOM_X_LEN, ROOM_Y_LEN, ROOM_Z_LEN = room_size[0], room_size[1], room_size[2]
 except:
     raise ImportError("ROOM_SIZE can't resolve, it should be like 5*5*3.")
 try:
     wall_node_num = list(map(int, WALL_NODE_NUM.split(",")))
+    WALL_NODE_NUM_X, WALL_NODE_NUM_Y, WALL_NODE_NUM_Z = \
+        wall_node_num[0], wall_node_num[1], wall_node_num[2]
 except:
     raise ImportError("WALL_NODE_NUM can't resolve, it should be like 10,10,10")
 
-
-ROOM_X_LEN, ROOM_Y_LEN, ROOM_Z_LEN = room_size[0], room_size[1], room_size[2]
-WALL_NODE_NUM_X, WALL_NODE_NUM_Y, WALL_NODE_NUM_Z = wall_node_num[0], \
-                                                    wall_node_num[1], \
-                                                    wall_node_num[2]
 MARGIN_X, MARGIN_Y, MARGIN_Z = (ROOM_X_LEN / WALL_NODE_NUM_X) / 2, \
                                (ROOM_Y_LEN / WALL_NODE_NUM_Y) / 2, \
                                (ROOM_Z_LEN / WALL_NODE_NUM_Z) / 2
